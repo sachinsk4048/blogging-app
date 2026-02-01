a2b0f1cf71fd6e364f32df64e7e20794138470c1
sachin <sachinsk30158@gmail.com>
2026-02-01 21:20:37 +0530

    added ban & unban user

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -34,3 +34,38 @@ exports.banUser = async (req, res) => {
   }
 };
 
+
+exports.unbanUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    if (!user.isBanned) {
+      return res.status(400).json({
+        message: "User is already Unbanned"
+      });
+    }
+
+    user.isBanned = false;
+    user.bannedAt = null;
+
+    await user.save();
+
+    return res.status(200).json({
+      message: "User Unbanned successfully"
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
